13bbc7f9c07e2e100116a3d200b8df25b09c75ed
Brady Soglin <bsoglin@gmail.com>
2021-01-24 21:37:29 -0600

    added history

--- Assets/UnityCommandLine/UnityCommandLine.cs
+++ Assets/UnityCommandLine/UnityCommandLine.cs
@@ -13,7 +13,9 @@ public class UnityCommandLine : MonoBehaviour
 
     public bool startHidden = false;
     bool hidden;
-    
+
+    List<string> history = new List<string>();
+    int historyIndex = -1;
 
     private void Awake() {
         response.text = "";
@@ -21,18 +23,43 @@ public class UnityCommandLine : MonoBehaviour
     }
 
     private void Update() {
+
+        // submit text
         if (Input.GetKeyDown(KeyCode.Return)) {
             OnSubmit(inputField.text);
         }
 
+        // gow up or down thru history
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            HandleHistoryChange(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            HandleHistoryChange(-1);
+        }
+
+        // hide console
         if (Input.GetKeyDown(showHideKey)) {
             ShowOrHide(!hidden);
+        }
+    }
+
+    // page up or down through the commands that were given previously
+    void HandleHistoryChange(int amount) {
+        historyIndex = Mathf.Clamp(historyIndex + amount, -1, history.Count - 1);
+
+        if (historyIndex < 0) {
+            inputField.SetTextWithoutNotify("");
+        } else {
+            inputField.SetTextWithoutNotify(history[historyIndex]);
         }
     }
 
     public void OnSubmit(string input) {
         response.text = "Could not parse command.";
 
+
+        // handle the respond coming in and send it to the receiver
         string[] inputSplit = input.Split(' ');
 
         if (inputSplit.Length == 1) {
@@ -45,6 +72,11 @@ public class UnityCommandLine : MonoBehaviour
             response.text = "could not parse command.";
         }
 
+        // update history
+        historyIndex = -1;
+        history.Insert(0, input);
+
+        // empty the input and select it
         inputField.SetTextWithoutNotify("");
         inputField.ActivateInputField();
     }
